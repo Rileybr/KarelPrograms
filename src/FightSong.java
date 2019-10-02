795783822574c36f235f7c4bb0576b47405de07f
rileybr <brandon.riley@ssfs.org>
2019-10-02 08:45:12 -0400

    got rid of extra repetition by using another method

--- src/FightSong.java
+++ src/FightSong.java
@@ -1,16 +1,22 @@
 public class FightSong {
+    private static void blank_space(){
+        System.out.println();
+    }
+
     private static void first_verse(){
-        System.out.println("Go, team, go! \nYou can do it.\n");
+        System.out.println("Go, team, go! \nYou can do it.");
     }
 
     private static void second_verse(){
-        System.out.println("Go, team, go! \nYou can do it.");
+        first_verse();
         System.out.println("You're the best, \nIn the West.");
-        System.out.println("Go, team, go! \nYou can do it.\n");
+        first_verse();
+        blank_space();
     }
 
     public static void main(String [] args){
         first_verse();
+        blank_space();
         second_verse();
         second_verse();
         first_verse();
